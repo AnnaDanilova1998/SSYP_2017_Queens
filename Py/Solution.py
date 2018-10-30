6dc2840c69cf5d74111bc0657123d2153a1878c8
German Yakimov <german13yakimov@gmail.com>
2018-10-30 20:26:38 +0700

    Implement class Solution (generating and errors counting).

--- Py/Solution.py
+++ Py/Solution.py
@@ -3,14 +3,61 @@ import random
 
 class Solution:
     def __init__(self, n):
-        self.errors_number = 0
-        self.genotype = []
+        self.hits = 0
+        self.genotype = [i for i in range(n)]
 
-        self._generate(n)
+        random.shuffle(self.genotype)
         self._count_errors_number()
 
-    def _generate(self, n):
-        self.genotype = random.shuffle(range(n))
-
     def _count_errors_number(self):
-        pass
+        size = len(self.genotype)
+
+        for i in range(size):
+
+            # lower right
+
+            x, y = i + 1, self.genotype[i] + 1
+
+            while 0 <= x < size and 0 <= y < size:
+                if self.genotype[x] == y:
+                    self.hits += 1
+                    break
+                else:
+                    x += 1
+                    y += 1
+
+            # lower left
+
+            x, y = i + 1, self.genotype[i] - 1
+
+            while 0 <= x < size and 0 <= y < size:
+                if self.genotype[x] == y:
+                    self.hits += 1
+                    break
+                else:
+                    x += 1
+                    y -= 1
+
+            # top right
+
+            x, y = i - 1, self.genotype[i] + 1
+
+            while 0 <= x < size and 0 <= y < size:
+                if self.genotype[x] == y:
+                    self.hits += 1
+                    break
+                else:
+                    x -= 1
+                    y += 1
+
+            # top left
+
+            x, y = i - 1, self.genotype[i] - 1
+
+            while 0 <= x < size and 0 <= y < size:
+                if self.genotype[x] == y:
+                    self.hits += 1
+                    break
+                else:
+                    x -= 1
+                    y -= 1
